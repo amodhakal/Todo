d1efd954aac1b8f8b5ea66593ea096ece1e579ea
Amodh Dhakal <amodhakal@AM1.local>
2023-02-13 19:16:53 -0500

    React Router Setup

--- src/client/src/main.tsx
+++ src/client/src/main.tsx
@@ -1,7 +1,28 @@
+import "./style.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routers/Home";
+import Login from "./routers/Login";
+import Register from "./routers/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
