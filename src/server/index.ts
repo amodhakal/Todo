f7f23ee771ff40cf2f3c085c6f58c01459925aec
Amodh Dhakal <amodhakal@gmail.com>
2023-02-18 12:12:09 -0500

    Some improvements

--- src/server/index.ts
+++ src/server/index.ts
@@ -17,8 +17,8 @@ const app = express();
 app.use(express.json());
 app.use(
   RateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // limit each IP to 100 requests per windowMs
+    windowMs: 15 * 60 * 1000,
+    max: 100,
   })
 );
 
